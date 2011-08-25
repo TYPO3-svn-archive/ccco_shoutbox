90fdec832e491ea193698fb52288531b9eba6272
tomeccco <tomeccco@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-25 11:31:46 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ccco_shoutbox/trunk@51351 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ccco_shoutbox/ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "ccco_shoutbox".
+#
+# Auto generated 07-06-2011 21:53
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'A Shoutbox realized with extbase and ajax',
+	'description' => 'A Shoutbox realised with extbase and ajax',
+	'category' => 'example',
+	'shy' => 0,
+	'version' => '0.0.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'tomeccco@blauerzauber.org',
+	'author_email' => '',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
+
+?>
