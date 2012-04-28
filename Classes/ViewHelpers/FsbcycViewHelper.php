d3860569326bd1739da0f798dbed40774fe2ecae
tomeccco <tomeccco@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-28 16:53:13 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/ccco_shoutbox/trunk@61235 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/ViewHelpers/FsbcycViewHelper.php
+++ Classes/ViewHelpers/FsbcycViewHelper.php
@@ -1,13 +1,22 @@
 <?php
 
 /***************************************************************
- *  Copyright notice
+ *  Copyright notice                                                                                                 
+ *      .                                                                                              
+ *  .MMMM    .   ...                                                                                   
+ *  .MMMM.. ZM.  ZM.                                                                                   
+ *   .MM8 =7MMM=, D       . ... ..  .  .... .... ..   ..    ..       ..  .  .     .  ..                
+ *       MMMMMMMM. .     .M M   M  M  . .?. M  ...    M     .M.      ,M  8   N M  M M ,                
+ *    .?MMMMMM+MMMM.     .M.M  +.. .  MM..  M.  M     M    .M?.      D ?.   Z M    7.                  
+ *     :MMMM+MMMMMM.     Z    .M..Z. .  M   :.. M   . . .  M..7      ..OM   M. M   M.                  
+ *        MMMMMMM M~                                                                                    
+ *     ,M  +MM.7M.                                                                                     
+ *      ?M   7M.   
+ *      2012 copyright by pusilla.net // tomeccco <info@pusilla.net>  
+ *      All rights reserved  
  *
- *  (c) 2011 Tom Bocek <info@pusilla.net>
- *  All rights reserved
- *
- *  This script is my first extbase project. The extbase project is
- *  free software; you can redistribute it and/or modify
+ *  This script is my first extbase project. 
+ *  It's free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 2 of the License, or
  *  (at your option) any later version.
